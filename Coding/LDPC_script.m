88b32af0462f0216e4f8d56763cae52abf34cdb6
PeshX <marcopascarella187@gmail.com>
2023-04-03 14:41:41 +0200

    Generator matrix for encoding added
    To be checked and validated

--- Coding/LDPC_script.m
+++ Coding/LDPC_script.m
@@ -1,4 +1,4 @@
-% Coding script #1
+% LDPC codes performance simulator
 
 clc
 clear
@@ -19,10 +19,17 @@ phi_matrix=[phi_0M phi_1M phi_2M phi_3M];
 H=[zeroM zeroM oneM zeroM xor(oneM,PermMatrix(1,theta_k,phi_matrix));
     oneM oneM zeroM oneM xor(xor(PermMatrix(2,theta_k,phi_matrix),PermMatrix(3,theta_k,phi_matrix)),PermMatrix(4,theta_k,phi_matrix));
     oneM xor(PermMatrix(5,theta_k,phi_matrix),PermMatrix(6,theta_k,phi_matrix)) zeroM xor(PermMatrix(7,theta_k,phi_matrix),PermMatrix(8,theta_k,phi_matrix)) oneM];
-% N.B. Last M columns of H shall be punctured.
+% N.B. Last M columns of H shall be punctured later.
 
 %% Generator matrix construction: data from Blue Book -> pag. 57-58 %%
-
+K=2;
+P=H(1:end,2*M+1:end); % Pick last 3M columns of H
+Q=H(1:end,1:K*M); % Pick first MK columns of H
+W=mod(floor(mod(inv(P),2))*Q,2)';
+% G matrix composition
+G=[eye(K*M,K*M) W];
+% Last M columns shall be punctured to obtain n=2048
+G=G(1:end,1:4*M);
 
 %% Random bit generation %%
 
