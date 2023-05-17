6f760c6292e5983788134416d4b9d7eead1cf4f5
Mahyar Onsori <mahiaronsori@gmail.com>
2023-05-17 21:05:01 +0200

    Perfect Plot Update

--- Ranging/PN_SEQUENCE.m
+++ Ranging/PN_SEQUENCE.m
@@ -1,58 +1,42 @@
-%% PN_SEQUENCE 
+%% PN_SEQUENCE
 
 clc;
 clear;
 close all;
 
 
-%% THIS IS A TEST 
-count=1;
-while(1)
-    if(count==1)
-        Y_1 = circshift([0,1],1); % K = numero di posizioni shiftate
-        Y_2 = circshift([0,0,0,1,1,0,1],1);
-        Y_3 = circshift([0,0,0,1,1,1,0,1,0,0,1],1);
-        Y_4 = circshift([0,0,0,0,1,1,1,0,1,1,0,0,1,0,1],1);
-        Y_5 = circshift([0,0,0,0,1,0,1,0,1,1,1,1,0,0,1,0,0,1,1],1);
-        Y_6 = circshift([0,0,0,0,0,1,0,1,0,0,1,1,0,0,1,1,0,1,0,1,1,1,1],1);
+%% THIS IS A TEST
+Counter=1;
+C1 = [+1 -1];
+C2 = [+1 +1 +1 -1 -1 +1 -1];
+C3 = [+1 +1 +1 -1 -1 -1 +1 -1 +1 +1 -1];
+C4 = [+1 +1 +1 +1 -1 -1 -1 +1 -1 -1 +1 +1 -1 +1 -1];
+C5 = [+1 +1 +1 +1 -1 +1 -1 +1 -1 -1 -1 -1 +1 +1 -1 +1 +1 -1 -1];
+C6 = [+1 +1 +1 +1 +1 -1 +1 -1 +1 +1 -1 -1 +1 +1 -1 -1 +1 -1 +1 -1 -1 -1 -1];
+regLen  = [2 7 11 15 19 23];
+codeLen = prod(regLen);
+while(Counter<=codeLen)
+    C1 = circshift(C1,1);
+    C2 = circshift(C2,1);
+    C3 = circshift(C3,1);
+    C4 = circshift(C4,1);
+    C5 = circshift(C5,1);
+    C6 = circshift(C6,1);
+    Sum_vect(Counter)=(4*C1(1))+C2(1)-C3(1)-C4(1)+C5(1)-C6(1); % the (1) is to sum only the last element otherwise the length is not the same and do not work
+    if (Sum_vect(Counter)>0)
+        output_vector(Counter) = 1;
     else
-        Y_1 = circshift(Y_1,1);
-        Y_2 = circshift(Y_2,1);
-        Y_3 = circshift(Y_3,1);
-        Y_4 = circshift(Y_4,1);
-        Y_5 = circshift(Y_5,1);
-        Y_6 = circshift(Y_6,1);
-    end 
-
-    Y_1_signed(count)=(Y_1(1)*(-2))+1;
-    Y_2_signed(count)=(Y_2(1)*(-2))+1;
-    Y_3_signed(count)=(Y_3(1)*(-2))+1;
-    Y_4_signed(count)=(Y_4(1)*(-2))+1;
-    Y_5_signed(count)=(Y_5(1)*(-2))+1;
-    Y_6_signed(count)=(Y_6(1)*(-2))+1;
-    Sum_vect(count)=(4*Y_1_signed(count))+Y_2_signed(count)+Y_3_signed(count)+Y_4_signed(count)+Y_5_signed(count)+Y_6_signed(count); % the (1) is to sum only the last element otherwise the length is not the same and do not work
-    if (Sum_vect(count)>0)
-        output_vector(count) = 1;
-    else  
-        output_vector(count) = 0;
-    end 
-
-    count=count +1 ; 
-    if (count > 10000)
-        break 
-    end 
-    
+        output_vector(Counter) = 0;
+    end
+    Counter=Counter +1 ;
 end
-
 % we compute and plot the autocorr func to check if it is correct
-symmInterval=5000;
+symmInterval=codeLen/2;
 R=ifft(fft(output_vector).*conj(fft(output_vector)));
 R_shifted=fftshift(R);
 max_1=max(R_shifted);
-R_shifted_normalized=R_shifted/max_1;
+min_1=min(R_shifted);
+R_shifted_normalized=(R_shifted-min_1)/(max_1-min_1);
 tau=-symmInterval:1:symmInterval-1;
-figure,plot(tau,R_shifted_normalized),grid on,axis('padded');
-ylim([0.9 1]);
-
-
-
+figure,plot(tau,abs(R_shifted_normalized)),grid on,axis('padded');
+ylim([0.87 1]);
