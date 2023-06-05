851047e35720babaccc7621cd800f940f02704b0
PeshX <marcopascarella187@gmail.com>
2023-06-05 22:46:57 +0200

    Added  simulation parameters

--- OTFS/OTFS_script.m
+++ OTFS/OTFS_script.m
@@ -5,3 +5,21 @@ clear
 close all
 
 %% Simulation parameters
+% Rows and columns of the DD space
+M=64;
+N=16;
+% Compute normalized N-DFT matrix (for the columns)
+Fn=dftmtx(N);
+Fn=Fn/norm(Fn);
+% Carrier frequency
+fc=4e+09;
+% Sub-carrier spacing
+deltaF=15e+03;
+% Block duration
+T=1/deltaF;
+% Speed of light
+c=299792458;
+% OTFS grid delay and Doppler resolutions
+delayRes=1/(M*deltaF);
+DopplerRes=1/(N*T);
+%% OTFS Modulator
