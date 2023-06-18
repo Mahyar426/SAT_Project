f2200e87bab17d53aa4542a8ad98332fe7ac40ec
PeshX <marcopascarella187@gmail.com>
2023-06-18 16:57:01 +0200

    Create Ranging_Simulation.m

---
+++ Ranging/Ranging_Simulation.m
@@ -0,0 +1,29 @@
+% Simulation for delay estimation through 2D-correlation
+
+clc
+clear
+close all
+
+%% Initializing circular shift registers
+C1 = [+1 -1];
+C2 = [+1 +1 +1 -1 -1 +1 -1];
+C3 = [+1 +1 +1 -1 -1 -1 +1 -1 +1 +1 -1];
+C4 = [+1 +1 +1 +1 -1 -1 -1 +1 -1 -1 +1 +1 -1 +1 -1];
+C5 = [+1 +1 +1 +1 -1 +1 -1 +1 -1 -1 -1 -1 +1 +1 -1 +1 +1 -1 -1];
+C6 = [+1 +1 +1 +1 +1 -1 +1 -1 +1 +1 -1 -1 +1 +1 -1 -1 +1 -1 +1 -1 -1 -1 -1];
+counter=1;
+regLen=[2 7 11 15 19 23];
+codeLen=prod(regLen);
+%% PN sequence generation
+Code=zeros(codeLen,1);
+while counter<=codeLen
+    Code(counter)=sign(4*C1(1)+C2(1)-C3(1)-C4(1)+C5(1)-C6(1));
+    C1 = circshift(C1,1);
+    C2 = circshift(C2,1);
+    C3 = circshift(C3,1);
+    C4 = circshift(C4,1);
+    C5 = circshift(C5,1);
+    C6 = circshift(C6,1);
+    counter=counter+1;
+end
+%% Channel model
