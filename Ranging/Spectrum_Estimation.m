bace71a8bd6675139542823e12cb27416f59d3f5
PeshX <marcopascarella187@gmail.com>
2023-07-16 18:46:27 +0200

    Renamed files for documentation

--- Ranging/Spectrum_Estimation.m
+++ Ranging/Spectrum_Estimation.m
@@ -4,7 +4,7 @@ clc
 clear
 close all
 
-%% Initializing circular shift registers ==================================
+%% Initializing circular shift registers 
 C1 = [+1 -1];
 C2 = [+1 +1 +1 -1 -1 +1 -1];
 C3 = [+1 +1 +1 -1 -1 -1 +1 -1 +1 +1 -1];
@@ -14,7 +14,7 @@ C6 = [+1 +1 +1 +1 +1 -1 +1 -1 +1 +1 -1 -1 +1 +1 -1 -1 +1 -1 +1 -1 -1 -1 -1];
 counter=1;
 regLen=[2 7 11 15 19 23];
 codeLen=prod(regLen);
-%% PN sequence generation =================================================
+%% PN sequence generation
 Code=zeros(codeLen,1);
 while counter<=codeLen
     Code(counter)=sign(4*C1(1)+C2(1)-C3(1)-C4(1)+C5(1)-C6(1));
@@ -30,7 +30,7 @@ discreteTimePlot=50;
 figure,stem(Code(1:discreteTimePlot)),grid on,axis('padded');
 title('T4B Pseudo-Noise sequence','Interpreter','latex');
 xlabel('[n]','Interpreter','latex');
-%% Baseband signal generation =============================================
+%% Baseband signal generation 
 SpS=4;                      % Arbitrarily chosen here
 Fchip=2e+06;                % From CCSDS 414.1-B-2 - Table A-1
 signalRect=rectpulse(Code,SpS);
@@ -47,7 +47,7 @@ xline(0,'LineWidth',1.5,'LineStyle','--','Color','black');
 xlabel('Frequency [MHz]','Interpreter','latex');
 ylabel('Magnitude [dB]','Interpreter','latex');
 title('$\hat{P} (f)$ of rectangularly-shaped signal | Baseband','Interpreter','latex');
-%% RF-IF front-end operations =============================================
+%% RF-IF front-end operations
 freqIF=10e+06;              % From CCSDS 414.0-G-2 - Section 2.2.5
 freqSamplIF=30e+06;         % > 24 MHz from IF spectrum plot
 % Generating IF signal
@@ -70,47 +70,3 @@ xline(-10,'LineWidth',1.5,'LineStyle','--','Color',[0.8500 0.3250 0.0980]);
 xlabel('Frequency [MHz]','Interpreter','latex');
 ylabel('Magnitude [dB]','Interpreter','latex');
 title('$\hat{P} (f)$ at Intermediate Frequency (IF) | $f_{carrier}=$ $ 10$ $MHz$','Interpreter','latex');
-%% Search Space (SS) definition ===========================================
-L=length(Code);
-Ts=1/freqSamplIF;
-Tcoh=L*Ts;
-Ntau=L;
-binsTau=0:1:Ntau-1;
-freqDopplerMax=10e+03;
-deltaFreq=ceil(2/(3*Tcoh));
-binsDoppler=-freqDopplerMax:deltaFreq:freqDopplerMax;
-Nf=length(binsDoppler);
-return
-%% Channel model: (delay, Doppler) shifts =================================
-rng(0,'twister');
-minRange=1;
-% maxRangeDoppler=length(binsDoppler);
-maxRangeTau=L;
-% randNumDoppler=floor((maxRangeDoppler-minRange)*rand(1,1)+minRange)
-randNumDoppler=200;
-randNumTau=floor((maxRangeTau-minRange)*rand(1,1)+minRange);
-shiftDoppler=randNumDoppler;
-disp(['Doppler shift is: ',num2str(shiftDoppler),' Hz']);
-shiftTau=randNumTau;
-disp(['Delay shift is: ',num2str(shiftTau),' sequence bits']);
-%% Acquisition: (delay, Doppler) estimation ===============================
-% Channel contribution without AWGN
-signalRx=circshift(signalIF.*exp(2i*pi*shiftDoppler),shiftTau);
-% Local replica signal modulated at IF
-signalReplica=signalIF;
-% Cross-ambiguity function in the DD domain
-S=zeros(Nf,Ntau);
-for i=1:Nf
-    % Apply test Doppler shifts
-    signalTest=signalReplica.*exp(2i*pi*binsDoppler(i));
-    % Delay shifts dealed with FFT circular correlation
-    CAF=ifft(fft(signalTest,L).*conj(fft(signalRx,L)));
-    S(i,:)=abs(CAF').^2;
-end
-%% 3D plot for results evaluation =========================================
-% figure,S_Plot=mesh(S);
-% S_Plot.FaceColor = 'flat';
-
-
-
-
